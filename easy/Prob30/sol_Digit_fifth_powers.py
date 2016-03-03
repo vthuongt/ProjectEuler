8f604db2b54ca137200b74f6f1390d0483a609d7
Viet <Viet@LappyNEW.Speedport_W723_V_Typ_A_1_01_012>
2016-03-03 01:11:26 +0100

    with modulo operations faster as expected

--- easy/Prob30/sol_Digit_fifth_powers.py
+++ easy/Prob30/sol_Digit_fifth_powers.py
@@ -1,3 +1,4 @@
+import time
 
 def val(n):
     s = str(n)
@@ -6,11 +7,25 @@ def val(n):
         value += int(i)**5
     return value
 
+def val2(n):
+    value = 0
+    while n != 0:
+        value += (n%10)**5
+        n= n//10
+    return value
 # finding max value to test to
+
 nmax = 9
 while nmax < val(nmax):
     nmax = nmax*10+9
 
 #print(str(nmax)+">"+str(val(nmax)))
-
+start = time.time()
 print(sum([x for x in range(2,nmax+1) if val(x)==x]))
+end = time.time()
+print(sum([x for x in range(2,nmax+1) if val2(x)==x]))
+end2 = time.time()
+
+print("Time1: "+str(end-start))
+
+print("Time2: "+str(end2-end))
